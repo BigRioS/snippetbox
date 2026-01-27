2d51ff2f5d411ddc19b0aeb188bd7c3ae933eb73
bigrios19 <bigrios19@gmail.com>
2026-01-27 17:13:24 +0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,47 @@
+﻿package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv" // New import
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Header()["X-XSS-Protection"] = []string{"1; mode=block"}
+
+	w.Write([]byte(`{"name":"Alex"}`))
+}
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from create"))
+}
+
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Save Post"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+
+	log.Println("Starting server on :4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
